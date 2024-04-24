231753c3659f26e1a386d731eae4fe7e4fc195df
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-04-24 22:35:49 +0900

    fix: api 전역 instance 수정

--- src/pages/api/api.tsx
+++ src/pages/api/api.tsx
@@ -4,15 +4,15 @@ import { getToken } from 'utils/axiosInstance'
 
 const accessToken = getToken()
 
-console.log('accesstoken : ', accessToken)
+// console.log('accesstoken : ', accessToken)
 
 export const API = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API,
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: `Bearer ${accessToken}`,
-		'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_API,
-		'Access-Control-Allow-Credentials': true,
+		// // Authorization: `Bearer ${accessToken}`,
+		// 'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_API,
+		// 'Access-Control-Allow-Credentials': true,
 	},
-	withCredentials: true,
+	// withCredentials: true,
 })
