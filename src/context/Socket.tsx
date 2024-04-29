0583002fecd5f391709f878e15b095c2fa93750d
idle-danie <daniel9654@ajou.ac.kr>
2024-04-29 13:38:27 +0900

    fix: API 엔드포인트 수정

--- src/context/Socket.tsx
+++ src/context/Socket.tsx
@@ -2,44 +2,48 @@ import { io } from 'socket.io-client'
 import { createContext, useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 
-const url = 'ws://localhost:3000'
+const url = 'ws://api.damdam.chat'
 const defualtState = {
-	socket: null,
-	connected: false,
-	reconnect: () => {},
+    socket: null,
+    connected: false,
+    reconnect: () => {},
 }
 
 export const SocketContext = createContext<any>(defualtState)
 export const SocketProvider = ({ children }: { children: JSX.Element }) => {
-	const [socket, setSocket] = useState<any>(defualtState.socket)
-	const [connected, setConnected] = useState<boolean>(defualtState.connected)
+    const [socket, setSocket] = useState<any>(defualtState.socket)
+    const [connected, setConnected] = useState<boolean>(defualtState.connected)
 
-	const reconnect = (option: any) => {
-		setSocket(
-			io(url, {
-				secure: true,
-				transports: ['websocket'],
-				query: option,
-				// reconnection: true,
-				reconnectionDelay: 1000,
-				reconnectionAttempts: 3,
-			}),
-		)
-	}
+    const reconnect = (option: any) => {
+        setSocket(
+            io(url, {
+                secure: true,
+                transports: ['websocket'],
+                query: option,
+                // reconnection: true,
+                reconnectionDelay: 1000,
+                reconnectionAttempts: 3,
+            })
+        )
+    }
 
-	useEffect(() => {
-		if (!socket) return
-		socket.on('connect', () => setConnected(true))
-		socket.on('disconnect', () => setConnected(false))
-	}, [socket])
+    useEffect(() => {
+        if (!socket) return
+        socket.on('connect', () => setConnected(true))
+        socket.on('disconnect', () => setConnected(false))
+    }, [socket])
 
-	useEffect(() => {
-		if (!connected) return console.log('[-] Socket Disconnected')
-		else console.log('[#] Socket Connected')
+    useEffect(() => {
+        if (!connected) return console.log('[-] Socket Disconnected')
+        else console.log('[#] Socket Connected')
 
-		socket.on('error', (data: any) => toast.error(data.msg))
-		socket.on('test', (data: any) => console.log('[*] Socket test', data))
-	}, [connected])
+        socket.on('error', (data: any) => toast.error(data.msg))
+        socket.on('test', (data: any) => console.log('[*] Socket test', data))
+    }, [connected])
 
-	return <SocketContext.Provider value={{ socket, connected, reconnect }}>{children}</SocketContext.Provider>
+    return (
+        <SocketContext.Provider value={{ socket, connected, reconnect }}>
+            {children}
+        </SocketContext.Provider>
+    )
 }
